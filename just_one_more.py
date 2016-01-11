a7e791ea59539414d0c84e2d93626a7905dd5fd6
Øystein Hofvind-Rom <oystein@hofvind-rom.com>
2016-01-11 01:26:24 +0100

    All 3 levels pass. But it needs refactoring.

--- just_one_more.py
+++ just_one_more.py
@@ -1,4 +1,36 @@
+import re
+
+
 def increment(values):
-    for index, item in enumerate(values):
-        values[index] = item + 1
-    return values
+    new_values = []
+    for item in values:
+        try:
+            item += 1
+            new_values.append(item)
+        except TypeError:
+            try:
+                if item.isalpha():
+                    continue
+                item = int(item) + 1
+                new_values.append(item)
+            except ValueError:
+                elements = re.split(r'(\D+)(\d+)(\D+)?(\d+)?', item)
+                new_item = ''
+                for entry in elements:
+                    if entry is None:
+                        continue
+                    elif entry == '':
+                        continue
+                    else:
+                        if entry.isdigit():
+                            if re.match(r'[0]\d', entry):
+                                entry = int(entry) + 1
+                                new_item += '0' + str(entry)
+                            else:
+                                entry = int(entry) + 1
+                                new_item += str(entry)
+                        else:
+                            new_item += entry
+                new_values.append(new_item)
+
+    return new_values
